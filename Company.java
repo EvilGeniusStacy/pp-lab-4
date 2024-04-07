06c032b042f15a4335c57b04bd32b14130177d33
Robert Matuszewski <robert.matuszewski556@gmail.com>
2024-04-07 09:44:14 +0200

    h

--- Company.java
+++ Company.java
@@ -1,5 +1,6 @@
 import employees.Employee;
 import employees.Manager;
+import employees.Worker;
 
 public class Company {
 
@@ -7,11 +8,11 @@ public class Company {
        
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Employee("pracownik 1", 2000.0);
-        employees[1] = new Employee("pracownik 2", 2500.0);
+        employees[0] = new Manager("pracownik 1", 2000.0, 0);
+        employees[1] = new Worker("pracownik 2", 2500.0, "stanowisko 1");
         employees[2] = new Employee("pracownik 3", 3000.0);
-        employees[3] = new Employee("pracownik 4", 3500.0);
-        employees[4] = new Employee("pracownik 5", 4000.0);
+        employees[3] = new Worker("pracownik 4", 3500.0,"stanowisko 2");
+        employees[4] = new Worker("pracownik 5", 4000.0,"stanowisko 3");
 
    int nonManagerCount = 0;
    for (Employee employee : employees) {
