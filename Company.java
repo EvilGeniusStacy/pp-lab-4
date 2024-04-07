a935d81c430c49f0a419d06414851d6e4a80f761
Robert Matuszewski <robert.matuszewski556@gmail.com>
2024-04-07 10:11:45 +0200

    h

--- Company.java
+++ Company.java
@@ -6,24 +6,23 @@ public class Company {
 
     public static void main(String[] args) {
        
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
         employees[0] = new Manager("pracownik 1", 2000.0, 0);
         employees[1] = new Worker("pracownik 2", 2500.0, "stanowisko 1");
         employees[2] = new Employee("pracownik 3", 3000.0);
         employees[3] = new Worker("pracownik 4", 3500.0,"stanowisko 2");
         employees[4] = new Worker("pracownik 5", 4000.0,"stanowisko 3");
+        employees[5] = new Manager("pracownik 6", 7000.0, 0);
+        employees[6] = new Worker("pracownik 7", 5000.0,"stanowisko 4");
 
-   int nonManagerCount = 0;
-   for (Employee employee : employees) {
-       if (!(employee instanceof Manager)) {
-           nonManagerCount++;
+        for (Employee employee : employees) {
+            employee.setSalary(employee.getSalary()+500);
+            if (employee instanceof Manager) {
+                ((Manager) employee).setNumberOfSubordinates(2);
+                employee.setSalary(75000);
        }
    }
-   ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
-
-   employees[0].setSalary(7500.0);
-
    System.out.println("Dane wszystkich pracowników:");
    for (Employee employee : employees) {
        System.out.println(employee);
